285c53bc5416fa592fbdeb854191e6b2ce536f69
Thiago promo <cthiagopromo@gmail.com>
2025-08-18 19:51:16 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/app/editor/[projectId]/page.tsx
+++ src/app/editor/[projectId]/page.tsx
@@ -20,7 +20,7 @@ export default function EditorPage() {
     if (foundProject) {
       setActiveProject(foundProject.id);
     }
-  }, [projectId, getProjectById, setActiveProject]);
+  }, [projectId, getProjectById]);
   
   if (project === undefined) {
     return (
